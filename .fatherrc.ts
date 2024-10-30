6bd45131565275109d0b522ad0e67d25ed7e7236
sorrycc <sorrycc@gmail.com>
2024-10-30 14:50:27 +0800

    fix: client/router missing

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -3,11 +3,6 @@ import { defineConfig } from 'father';
 export default defineConfig({
   cjs: {
     input: 'src',
-    ignores: ['src/client/**'],
     output: 'dist',
-  },
-  esm: {
-    input: 'src/client',
-    output: 'client',
   },
 });
