8d8edb8e0a653c29042f439c6ae11207938c63d9
Wu-kung <1434246346@qq.com>
2024-11-11 10:54:16 +0800

    ci: pnpm-lock.yaml-settings.autoInstallPeers

--- examples/hackernews/src/components/comment/index.tsx
+++ examples/hackernews/src/components/comment/index.tsx
@@ -1,12 +1,9 @@
-import React, { useState } from 'react';
-import { Link } from '@umijs/tnf/router';
+import React, { Link } from '@umijs/tnf/router';
 import type { CommentType } from '../../types';
 import { timeAgo } from '../../utils';
 import styles from './index.module.less';
 
 export default function Comment({ comment }: { comment: CommentType }) {
-  const [open, setOpen] = useState(true);
-
   if (!comment) return null;
 
   return (
@@ -14,32 +11,11 @@ export default function Comment({ comment }: { comment: CommentType }) {
       <div className={styles.by}>
         <Link to={`/user/${comment.by}`}>{comment.by}</Link>
         <span>{` ${timeAgo(comment.time)} ago`}</span>
-        {comment.kids ? (
-          <span>
-            {' '}
-            |{' '}
-            <a
-              href=""
-              className={styles.expand}
-              onClick={(e) => {
-                e.preventDefault();
-                setOpen(!open);
-              }}
-            >
-              {`${open ? 'collapse' : 'expand'} ${comment.kids.length}`}
-            </a>
-          </span>
-        ) : null}
       </div>
       <div
         className={styles.text}
         dangerouslySetInnerHTML={{ __html: comment.text }}
       />
-      <div className={styles.commentChildren}>
-        {comment.kids && open
-          ? comment.kids.map((id) => <Comment key={id} id={id} />)
-          : null}
-      </div>
     </div>
   );
 }
