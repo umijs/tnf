6e7ffd75436474295596aabb9191fb791565e560
Jinx <61403813+Jokergga@users.noreply.github.com>
2024-11-13 23:15:47 +0800

    fix(sync): configuration generation error in tsconfig.json (#40)
    * fix: configuration generation error in tsconfig.json
    
    * Update src/sync/write_types.ts
    
    ---------
    
    Co-authored-by: 张建华(zhangjh) <xxx>
    Co-authored-by: chencheng (云谦) <sorrycc@gmail.com>

--- src/sync/write_types.ts
+++ src/sync/write_types.ts
@@ -40,7 +40,7 @@ export function writeTypes({ context }: SyncOptions) {
     "module": "esnext",
     "moduleResolution": "bundler",
     "noEmit": true,
-    "s": true,
+    "strictNullChecks": true,
     "target": "esnext"
   },
   "include": [
