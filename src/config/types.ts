fe9d4d9e5ed717a4e9b7f9209952486af84910eb
sorrycc <sorrycc@gmail.com>
2024-11-14 16:21:16 +0800

    feat: add bundler config, but the webpack bundler is not implemented

--- src/config/types.ts
+++ src/config/types.ts
@@ -3,6 +3,7 @@ import { PluginSchema } from '../plugin/types';
 
 export const ConfigSchema = z.object({
   alias: z.array(z.tuple([z.string(), z.string()])).optional(),
+  bundler: z.enum(['webpack', 'mako']).optional(),
   externals: z.record(z.string()).optional(),
   devServer: z
     .object({
