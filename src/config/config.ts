22ce5ffc07cf8ced20fec9d9c781775c616b8005
xiaohuoni <xiaohuoni@dingtalk.com>
2024-11-14 12:03:01 +0800

    feat: add config command

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,8 +2,10 @@ import {
   loadConfig as loadC12Config,
   watchConfig as watchC12Config,
 } from 'c12';
+import { updateConfig } from 'c12/update';
+import pc from 'picocolors';
 import { CONFIG_FILE } from '../constants';
-import type { Config } from './types';
+import type { Config, Context } from './types';
 import { ConfigSchema } from './types';
 
 interface ConfigOpts {
@@ -47,4 +49,107 @@ function createLoadConfigOpts({ cwd, defaults, overrides }: ConfigOpts) {
     defaults,
     overrides,
   };
+}
+
+export function list(config: Config, name?: string) {
+  const getValue = (value: any) => {
+    if (typeof value !== 'function') {
+      return value;
+    }
+    return pc.yellow('The value data type does not support the view');
+  };
+  const print = (key: string) => {
+    console.log(
+      ` - ${pc.blue(`[key: ${key}]`)}`,
+      getValue(config[key as keyof Config]),
+    );
+    console.log();
+  };
+  console.log();
+  console.log(`  Configs:`);
+  console.log();
+  if (name) {
+    if (!config[name as keyof Config]) {
+      // current key not existed
+      throw new Error(`key '${name}' not found`);
+    }
+    print(name as string);
+  } else {
+    // list all
+    Object.keys(config).forEach((key) => {
+      print(key);
+    });
+  }
+
+  console.log();
+}
+
+export async function setConfig({
+  cwd,
+  name,
+  value,
+  type = 'set',
+}: {
+  cwd: string;
+  name: string | number | undefined;
+  value: any;
+  type?: string | 'set' | 'remove';
+}) {
+  const configOpts = createLoadConfigOpts({ cwd });
+  await updateConfig({
+    ...configOpts,
+    onUpdate: (_config) => {
+      if (!name) {
+        console.log(pc.yellow(`key '${name}' not found`));
+        return;
+      }
+      const _value = _config[name];
+      if (type === 'remove') {
+        if (_config.hasOwnProperty(name)) {
+          delete _config[name];
+        }
+      } else {
+        if (value) {
+          let safeValue: string | number | boolean | undefined = value;
+          if (safeValue === 'true') {
+            safeValue = true;
+          } else if (safeValue === 'false') {
+            safeValue = false;
+          }
+          _config[name] = safeValue as string | number | undefined;
+        }
+      }
+      const result = ConfigSchema.safeParse(_config);
+      if (!result.success) {
+        console.log(
+          pc.yellow(`Invalid configuration: ${result.error.message}`),
+        );
+        // Verification failed, recall modification
+        _config[name] = _value;
+      } else {
+        console.log(
+          pc.green(`${type} config:${name} on ${configOpts.configFile}`),
+        );
+      }
+    },
+  });
+}
+export async function config({ context }: { context: Context }) {
+  const { _ } = context.argv;
+  const [, command, name, value] = _;
+
+  switch (command) {
+    case 'list':
+      list(context.config);
+      break;
+    case 'get':
+      list(context.config, `${name}`);
+      break;
+    case 'set':
+    case 'remove':
+      setConfig({ cwd: context.cwd, name, value, type: command });
+      break;
+    default:
+      throw new Error(`Unsupported sub command ${command} for tnf config.`);
+  }
 }
