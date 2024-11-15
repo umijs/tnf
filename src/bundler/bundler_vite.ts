41a2c94b940224582b8c9788f59c0ebfa5c984c7
WQ <wq990217@gmail.com>
2024-11-15 13:57:22 +0800

    feat:update vite version

--- src/bundler/bundler_vite.ts
+++ src/bundler/bundler_vite.ts
@@ -1,16 +1,18 @@
 import type { Bundler } from './bundler';
+import path from 'path';
+import { pathToFileURL } from 'url';
 
 export default {
   build: async (opts) => {
-    const { bundlerConfig, cwd, watch } = opts;
+    const { bundlerConfig, cwd } = opts;
     try {
-      // @ts-ignore
-      const { build: bundlerVite } = await import(
-        require.resolve('@umijs/bundler-vite', {
-          paths: [cwd],
-        })
-      ).catch(() => {
-        throw new Error('Please install first @umijs/bundler-vite');
+      // 直接定位到 vite 包的 ESM 入口
+      const viteModulePath = pathToFileURL(
+        path.join(cwd, 'node_modules', 'vite', 'dist', 'node', 'index.js')
+      ).href;
+
+      const { build } = await import(viteModulePath).catch((e) => {
+        throw new Error('Failed to import Vite, please install first vite@^6');
       });
 
       // 转换 alias 格式
@@ -21,22 +23,31 @@ export default {
         });
       }
 
-      const config = {
+      await build({
         resolve: {
           alias: aliasConfig,
         },
-        ...bundlerConfig,
-      };
-
-      await bundlerVite({
-        entry: bundlerConfig.entry,
-        config,
-        cwd,
-        watch: Boolean(watch),
+        mode: bundlerConfig.mode,
+        build: {
+          assetsDir: '',
+          rollupOptions: {
+            output: {
+              entryFileNames: '[name].js',
+              chunkFileNames: '[name].js',
+              assetFileNames: '[name].[ext]'
+            },
+            input: bundlerConfig.entry,
+          }
+        },
+        css: {
+          preprocessorOptions: {
+            less: bundlerConfig.less
+          },
+        },
       });
     } catch (err) {
       if (err instanceof Error && err.message.includes('Cannot find module')) {
-        throw new Error('Please install first @umijs/bundler-vite');
+        throw new Error('Failed to import Vite, please install first vite@^6');
       }
       throw err;
     }
