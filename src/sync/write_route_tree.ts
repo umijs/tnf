8aaebb01c8cc5958dbbf6ad07bdc1cdcb90b2194
sorrycc <sorrycc@gmail.com>
2024-12-25 16:42:56 +0800

    dep: upgrade to latest

--- src/sync/write_route_tree.ts
+++ src/sync/write_route_tree.ts
@@ -10,29 +10,32 @@ export async function writeRouteTree({ context }: SyncOptions) {
   } = context;
   const { router } = config;
 
-  await generator({
-    routeFileIgnorePrefix: '-',
-    routesDirectory: path.join(cwd, 'src/pages'),
-    generatedRouteTree: path.join(tmpPath, 'routeTree.gen.ts'),
-    quoteStyle: 'single',
-    semicolons: false,
-    disableTypes: false,
-    addExtensions: false,
-    disableLogging: false,
-    disableManifestGeneration: false,
-    apiBase: '/api',
-    routeTreeFileHeader: [
-      '/* prettier-ignore-start */',
-      '/* eslint-disable */',
-      '// @ts-nocheck',
-      '// noinspection JSUnusedGlobalSymbols',
-    ],
-    routeTreeFileFooter: ['/* prettier-ignore-end */'],
-    indexToken: 'index',
-    routeToken: 'route',
-    experimental: {
-      enableCodeSplitting: true,
-    },
-    ...(router?.convention || {}),
-  } as Config);
+  await generator(
+    {
+      routeFileIgnorePrefix: '-',
+      routesDirectory: path.join(cwd, 'src/pages'),
+      generatedRouteTree: path.join(tmpPath, 'routeTree.gen.ts'),
+      quoteStyle: 'single',
+      semicolons: false,
+      disableTypes: false,
+      addExtensions: false,
+      disableLogging: false,
+      disableManifestGeneration: false,
+      apiBase: '/api',
+      routeTreeFileHeader: [
+        '/* prettier-ignore-start */',
+        '/* eslint-disable */',
+        '// @ts-nocheck',
+        '// noinspection JSUnusedGlobalSymbols',
+      ],
+      routeTreeFileFooter: ['/* prettier-ignore-end */'],
+      indexToken: 'index',
+      routeToken: 'route',
+      experimental: {
+        enableCodeSplitting: true,
+      },
+      ...(router?.convention || {}),
+    } as Config,
+    cwd,
+  );
 }
