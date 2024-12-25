db8fca6c3d2fb461d805bc43a9f2494a166623e9
chencheng (云谦) <sorrycc@gmail.com>
2024-12-25 13:52:57 +0800

    feat: improve create-tnf's style (#82)

--- create-tnf/src/create.ts
+++ create-tnf/src/create.ts
@@ -1,22 +1,27 @@
-import { intro, isCancel, outro, select, text } from '@clack/prompts';
+import { spawn } from 'child_process';
 import fs from 'fs';
 import path from 'path';
 import pc from 'picocolors';
+import { fileURLToPath } from 'url';
+import * as p from './clack/prompt/index.js';
 
 const CANCEL_TEXT = 'Operation cancelled.';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+type NpmClient = 'pnpm' | 'yarn' | 'npm';
 
 export async function create({
   cwd,
   name,
   template,
+  npmClient = 'pnpm',
 }: {
   cwd: string;
   name?: string;
   template?: string;
+  npmClient?: NpmClient;
 }) {
-  console.log();
-  intro(pc.bgCyan(' Creating a new TNF project... '));
-
   const templatesPath = path.join(__dirname, '../templates');
   const templateList = fs
     .readdirSync(templatesPath)
@@ -26,16 +31,18 @@ export async function create({
 
   const selectedTemplate =
     template ||
-    (await select({
-      message: 'Which template would you like?',
-      options: templateList.map((template) => ({
-        label: template,
-        value: template,
-      })),
-    }));
-
-  if (isCancel(selectedTemplate)) {
-    outro(pc.red(CANCEL_TEXT));
+    (await (async () => {
+      const template = await p.select({
+        message: 'Which template would you like?',
+        options: templateList.map((template) => ({
+          label: template,
+          value: template,
+        })),
+      });
+      return template;
+    })());
+  if (p.isCancel(selectedTemplate)) {
+    p.cancel(CANCEL_TEXT);
     return;
   }
 
@@ -47,26 +54,22 @@ export async function create({
       }
       return name;
     }
-
-    return await text({
+    return await p.text({
       message: 'Please enter a name for your new project:',
       initialValue: 'myapp',
       validate,
     });
-
     function validate(value: string) {
       if (!value) {
         return `Project name is required but got ${value}`;
       }
-
       if (fs.existsSync(path.join(cwd, value))) {
         return `Project ${path.join(cwd, value)} already exists`;
       }
     }
   })();
-
-  if (isCancel(projectName)) {
-    outro(CANCEL_TEXT);
+  if (p.isCancel(projectName)) {
+    p.cancel(CANCEL_TEXT);
     return;
   }
 
@@ -74,13 +77,75 @@ export async function create({
     throw new Error('Project already exists');
   }
 
+  const copySpinner = p.spinner();
+  copySpinner.start(`Copying template ${selectedTemplate}...`);
   const templatePath = path.join(templatesPath, selectedTemplate as string);
   const projectPath = path.join(cwd, projectName);
   fs.cpSync(templatePath, projectPath, { recursive: true });
-  outro(pc.green(`Project created in ${projectPath}`));
-  console.log(`Now run:
+  copySpinner.stop(`Copied template ${selectedTemplate}`);
 
-    cd ${projectName}
-    npm install
-    npm run build`);
+  const installTask = p.taskLog(`Installing dependencies with ${npmClient}...`);
+  const args = npmClient === 'yarn' ? [] : ['install'];
+  try {
+    await execa(npmClient, args, {
+      cwd: projectPath,
+      onData: (data) => {
+        installTask.text = data;
+      },
+    });
+  } catch (error) {
+    installTask.fail(`Failed to install dependencies with ${npmClient}`);
+    throw error;
+  }
+  installTask.success(`Installed dependencies with ${npmClient}`);
+
+  const syncTask = p.taskLog('Setting up project...');
+  try {
+    await execa('npx', ['tnf', 'sync'], {
+      cwd: projectPath,
+      onData: (data) => {
+        syncTask.text = data;
+      },
+    });
+  } catch (error) {
+    syncTask.fail(`Failed to setup project`);
+    throw error;
+  }
+  syncTask.success(`Project setup complete`);
+
+  p.box(
+    `
+1: ${pc.bold(pc.cyan(`cd ${projectName}`))}
+2: ${pc.bold(pc.cyan(`git init && git add -A && git commit -m "Initial commit"`))} (optional)
+3: ${pc.bold(pc.cyan(`${npmClient} run dev`))}
+
+To close the dev server, hit ${pc.bold(pc.cyan('Ctrl+C'))}
+    `.trim(),
+    'Next Steps',
+  );
+}
+
+async function execa(
+  cmd: string,
+  args: string[],
+  options: { cwd: string; onData: (data: string) => void },
+) {
+  const child = spawn(cmd, args, {
+    stdio: 'pipe',
+    cwd: options.cwd,
+  });
+  return new Promise((resolve, reject) => {
+    child.stdout?.on('data', (data) => {
+      options.onData(data);
+    });
+    child.stderr?.on('data', (data) => {
+      options.onData(data);
+    });
+    child.on('close', (code) => {
+      resolve(code);
+    });
+    child.on('error', (error) => {
+      reject(error);
+    });
+  });
 }
