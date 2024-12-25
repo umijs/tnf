e4c14ebd989e6f92160bf4538e4d49b76a1c0372
Wu-kung <1434246346@qq.com>
2024-12-25 16:55:29 +0800

    chore: bootstrapExample:use defineConfig in the .tnfrc.ts (#80)
    * feat: support mock.delay
    
    * chore: add changelog
    
    * chore: support url parameter
    
    * chore(mock): use this.config, no need to pass from mock()
    
    * chore: bootstrapExample:use defineConfig in the .tnfrc.ts
    
    * Update scripts/bootstrapExample.ts
    
    ---------
    
    Co-authored-by: chencheng (云谦) <sorrycc@gmail.com>

--- scripts/bootstrapExample.ts
+++ scripts/bootstrapExample.ts
@@ -60,8 +60,10 @@ import 'zx/globals';
     fs.writeFileSync(
       path.join(exampleDir, '.tnfrc.ts'),
       `
-export default {
-};
+import { defineConfig } from '@umijs/tnf';
+
+export default defineConfig({
+});
     `.trimStart(),
     );
     fs.mkdirSync(path.join(exampleDir, 'src'));
